231df593c38036a845fa4c26da61f05d35d390cd
Chiwai Yau <chiwai.yau@gravitysupplychain.com>
2021-11-26 23:29:20 +0800

    added show error on dev mode

--- dotnetweb/Program.cs
+++ dotnetweb/Program.cs
@@ -12,14 +12,18 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.UseSwagger();
-  app.UseSwaggerUI(options =>
+  app.UseDeveloperExceptionPage();
+}
+else
+{
+  app.UseHsts();
+}
+app.UseSwagger();
+app.UseSwaggerUI(options =>
   {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
     options.RoutePrefix = string.Empty;
   });
-}
-
 app.UseAuthorization();
 
 app.MapControllers();
